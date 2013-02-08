b383ce301f00753d56883bfe17ecf7cece3270a4
Todd Fredrich <tfredrich@gmail.com>
2013-02-08 16:01:36 -0700

    Updated mongodb scaffold to use 0.9.0-SNAPSHOT of RestExpress, which uses Jackson for JSON processing.

--- mongodb/src/main/java/com/strategicgains/restexpress/scaffold/mongodb/serialization/JsonSerializationProcessor.java
+++ mongodb/src/main/java/com/strategicgains/restexpress/scaffold/mongodb/serialization/JsonSerializationProcessor.java
@@ -1,27 +1,18 @@
 package com.strategicgains.restexpress.scaffold.mongodb.serialization;
 
-import java.util.Date;
-
 import org.bson.types.ObjectId;
 
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.strategicgains.restexpress.serialization.json.DefaultJsonProcessor;
-import com.strategicgains.restexpress.serialization.json.GsonTimestampSerializer;
-import com.strategicgains.util.date.DateAdapterConstants;
 
 public class JsonSerializationProcessor
 extends DefaultJsonProcessor
 {
-
-	public JsonSerializationProcessor()
+	@Override
+    protected void initializeModule(SimpleModule module)
     {
-	    super(
-	    	new GsonBuilder()
-			.disableHtmlEscaping()
-			.registerTypeAdapter(Date.class, new GsonTimestampSerializer())
-			.registerTypeAdapter(ObjectId.class, new GsonObjectIdSerializer())
-			.setDateFormat(DateAdapterConstants.TIMESTAMP_OUTPUT_FORMAT)
-			.create()
-	    );
+	    super.initializeModule(module);
+	    module.addDeserializer(ObjectId.class, new ObjectIdDeserializer());
+	    module.addSerializer(ObjectId.class, new ObjectIdSerializer());
     }
 }
