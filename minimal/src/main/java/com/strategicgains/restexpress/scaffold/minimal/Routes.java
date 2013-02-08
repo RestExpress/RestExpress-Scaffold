d6828955cbcfe15573d665343e673ea49603584a
Todd Fredrich <tfredrich@gmail.com>
2013-02-08 15:58:09 -0700

    Added ".{format}" to minimal scaffolding routes.

--- minimal/src/main/java/com/strategicgains/restexpress/scaffold/minimal/Routes.java
+++ minimal/src/main/java/com/strategicgains/restexpress/scaffold/minimal/Routes.java
@@ -10,11 +10,11 @@ public abstract class Routes
 	public static void define(Configuration config, RestExpress server)
     {
 		//TODO: Your routes here...
-		server.uri("/your/route/here/{sampleId}", config.getSampleController())
+		server.uri("/your/route/here/{sampleId}.{format}", config.getSampleController())
 			.method(HttpMethod.GET, HttpMethod.PUT, HttpMethod.DELETE)
 			.name(Constants.Routes.SINGLE_SAMPLE);
 
-//		server.uri("/your/collection/route/here", config.getSampleController())
+//		server.uri("/your/collection/route/here.{format}", config.getSampleController())
 //			.action("readAll", HttpMethod.GET)
 //			.method(HttpMethod.POST)
 //			.name(Constants.Routes.SAMPLE_COLLECTION);
