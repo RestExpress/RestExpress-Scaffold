93e629bc62a32a50a5d5fbed168245a81404053b
Todd Fredrich <tfredrich@gmail.com>
2014-09-05 16:40:43 -0600

    Removed archetype.properties files, updated create-archetype.sh to remove bin/ directories before building.

--- scripts/create-archetype.sh
+++ scripts/create-archetype.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 rm -rf .settings/
+find . -name bin | xargs rm -rf 
 mvn clean eclipse:clean archetype:create-from-project -Darchetype.properties=./archetype.properties
 
 sed -i .orig 's/org.restexpress.scaffold.cassandra/${package}/g' ./target/generated-sources/archetype/src/main/resources/archetype-resources/pom.xml
