a834ede86e4c44f923f2e5ca52750afba6e82a64
Todd Fredrich <tfredrich@gmail.com>
2014-09-05 16:07:04 -0600

    Updated for latest RestExpress release (0.10.4)

--- mongodb/src/main/java/org/restexpress/scaffold/mongodb/persistence/SampleOidEntityRepository.java
+++ mongodb/src/main/java/org/restexpress/scaffold/mongodb/persistence/SampleOidEntityRepository.java
@@ -2,14 +2,14 @@ package org.restexpress.scaffold.mongodb.persistence;
 
 import org.restexpress.scaffold.mongodb.domain.SampleOidEntity;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.strategicgains.repoexpress.mongodb.MongodbEntityRepository;
 
 public class SampleOidEntityRepository
 extends MongodbEntityRepository<SampleOidEntity>
 {
 	@SuppressWarnings("unchecked")
-    public SampleOidEntityRepository(Mongo mongo, String dbName)
+    public SampleOidEntityRepository(MongoClient mongo, String dbName)
 	{
 		super(mongo, dbName, SampleOidEntity.class);
 	}
--- mongodb/src/main/java/org/restexpress/scaffold/mongodb/persistence/SampleUuidEntityRepository.java
+++ mongodb/src/main/java/org/restexpress/scaffold/mongodb/persistence/SampleUuidEntityRepository.java
@@ -2,14 +2,14 @@ package org.restexpress.scaffold.mongodb.persistence;
 
 import org.restexpress.scaffold.mongodb.domain.SampleUuidEntity;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.strategicgains.repoexpress.mongodb.MongodbUuidEntityRepository;
 
 public class SampleUuidEntityRepository
 extends MongodbUuidEntityRepository<SampleUuidEntity>
 {
 	@SuppressWarnings("unchecked")
-    public SampleUuidEntityRepository(Mongo mongo, String dbName)
+    public SampleUuidEntityRepository(MongoClient mongo, String dbName)
 	{
 		super(mongo, dbName, SampleUuidEntity.class);
 	}
